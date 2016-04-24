22c9d5b5bf89bd065f1e4dabcb75f88250b7fb9d
KonstantinGrigorov <K.Grigorov@i.ua>
2016-04-24 18:51:33 +0300

    calculator and cookie

--- classwork/1/calculator.php
+++ classwork/1/calculator.php
@@ -4,8 +4,33 @@ require_once 'iCalculator.php';
 
 class Calculator implements iCalculator
 {
+public function plus($a, $b){
+	return $a+$b.'<br>';
+}
+
+public function minus($a, $b)
+{
+	return $a-$b.'<br>';
+}
+
+
+public function divide($a, $b){
+	if ($b==0)
+	{
+		echo "You input a zero! Input another number<br>";
+	}
+	else{
+	return $a/$b.'<br>';}
+}
+
+public function multiply($a, $b){
+	return $a*$b.'<br>';
+}
 
 }
 
 $calculator = new Calculator();
-$calculator->plus(1, 2); // 3
+echo $calculator->plus(1, 2); // 3
+echo $calculator->minus(1, 2);
+echo $calculator->divide(1, 0);
+echo $calculator->multiply(1, 2);
--- classwork/2/cookie.php
+++ classwork/2/cookie.php
@@ -7,16 +7,34 @@ require_once 'iCookie.php';
  * сохранение куки, удаление куки, считывание куки. Используйте ассоциативный массив $_COOKIE;
  */
 class Cookie implements iCookie
+
 {
+	
+	public function setCookie($key, $value)
+	{
+	setcookie($key, $value);
+	}
+
+	public function getCookie($key) 
+	{
+	return $_COOKIE[$key];
+	}
+
+	public function deleteCookie($key)
+	{
+	 setcookie($key, "",time() - 3600);
+	}
 
 }
 
+
 $cookie = new Cookie();
-$cookie->setCookie('key1', 'value1');
-$cookie->getCookie('key1'); // value1
-$cookie->deleteCookie('key1');
-$cookie->getCookie('key1'); // null
-
-//changes
-
-//changes2
+echo $cookie->getCookie('test'); 
+$cookie->deleteCookie('test');
+//$cookie->setCookie('test', 'some text');
+echo $cookie->getCookie('test'); // value1
+// $cookie->deleteCookie('test', 2);
+//$cookie->getCookie('test'); // null
+//echo $cookie->setCookie('key1', 'value1');
+//echo $cookie->getCookie('key1');
+//echo $cookie->deleteCookie('key1', '10');
