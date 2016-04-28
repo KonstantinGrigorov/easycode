3549d038bbc398fc385c087748c43ab415e615c4
Bogdan Solomykin <bogdan@Bogdans-MacBook-Pro.local>
2016-04-28 17:26:17 +0300

    * asd

---
+++ yii/models/Post.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $text
+ * @property string $date_creation
+ * @property integer $category_id
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['text'], 'string'],
+            [['date_creation'], 'safe'],
+            [['category_id'], 'integer'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'text' => 'Text',
+            'date_creation' => 'Date Creation',
+            'category_id' => 'Category ID',
+        ];
+    }
+}
