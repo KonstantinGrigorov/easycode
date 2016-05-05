ca35fedff25f4cb445dd7c5b7fd3e4d0467dd9c5
Bogdan Solomykin <bogdansolomykin@gmail.com>
2016-05-05 22:21:00 +0300

    2,3,4 tasks

--- yii/controllers/TestController.php
+++ yii/controllers/TestController.php
@@ -2,11 +2,14 @@
 
 namespace app\controllers;
 
+use app\models\Power;
+use app\models\Rectangle;
 use Yii;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 
 use app\models\TestOne;
+use app\models\Color;
 
 /**
  * PostController implements the CRUD actions for Post model.
@@ -32,4 +35,20 @@ class TestController extends Controller
        $model = new TestOne($_REQUEST, $_GET, $_POST);
        return $this->render('test1', ['model' => $model]);
    }
+
+    public function actionPower() {
+        $model = new Power();
+        return $this->render('power', ['model' => $model]);
+    }
+
+    public function actionFour() {
+        $color = new Color(50, 10, 100);
+        $rectangle = new Rectangle($color, 400, 400);
+        return $this->render(
+            'four',
+            [
+                'rectangle' => $rectangle
+            ]
+        );
+    }
 }
