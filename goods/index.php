8441e7c7967d7bbd7221529827f9431aa5914635
KonstantinGrigorov <K.Grigorov@i.ua>
2016-05-23 20:51:53 +0300

    html form

---
+++ goods/index.php
@@ -0,0 +1,26 @@
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>goods form</title>
+    
+</head>
+
+  <body>
+    <form action="handler.php">
+   <p><strong>Product name</strong></p>
+   <p><input name="product name" maxlength="25" size="40" ></p>
+  
+ <label> Product category</label>
+ <select name="category">
+  <option value="0" selected="selected"></option>
+  <option value="1">Toys</option>
+  <option value="2">Electronics</option>
+  <option value="3">Accesuars</option>
+</select>
+ <p><strong> Product cost</strong></p>
+   <p><input name="cost" maxlength="10" size="20" ></p>
+</form>
+    </body>
+
+</html>
